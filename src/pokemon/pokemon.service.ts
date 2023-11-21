a5aa607fb1274b5d3ad5bc6635726c323b906793
iverge93 <isaias.vergelin@gmail.com>
2023-11-21 18:18:45 -0300

    conexion a base de datos

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -1,11 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
+import { Model } from 'mongoose';
+import { Pokemon } from './entities/pokemon.entity';
+import { InjectModel } from '@nestjs/mongoose';
 
 @Injectable()
 export class PokemonService {
-  create(createPokemonDto: CreatePokemonDto) {
-    return 'This action adds a new pokemon';
+
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel : Model<Pokemon>
+  ){}
+
+  async create(createPokemonDto: CreatePokemonDto) {
+
+    try {
+      createPokemonDto.name = createPokemonDto.name.toLocaleLowerCase()
+
+      const pokemon = await this.pokemonModel.create(createPokemonDto);
+
+      return pokemon;
+    } catch (error) {
+      if(error.code == 11000) throw new BadRequestException(`El pokemon ya existe en la Base de Datos - ${JSON.stringify(error.keyValue)}`);
+      else throw new InternalServerErrorException();
+    }
+    
   }
 
   findAll() {
